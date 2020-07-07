57f1d637e6b497a48707999db5cd9ae3d49c20ff
FerhatGec <ferhatgectao@gmail.com>
2020-07-07 17:33:00 +0300

    Added first source.

---
+++ EasyMorse.cpp
@@ -0,0 +1,15 @@
+/* MIT License
+#
+# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# */
+
+#include <iostream>
+#include <string>
+#include "EasyMorse.hpp"
+
+int main() {
+	EasyMorse::MainMorse();
+	return 0;
+}
